a095b9fd62dca7c19c5e64b66432132e2abcbc75
duongna21 <lablaugrana01@gmail.com>
2022-07-25 23:17:54 +0700

    add bart pretraining script flax

--- examples/flax/language-modeling/run_bart_dlm_flax.py
+++ examples/flax/language-modeling/run_bart_dlm_flax.py
@@ -522,6 +522,8 @@ def main():
     # For CSV/JSON files, this script will use the column called 'text' or the first column if no column called
     # 'text' is found. You can easily tweak this behavior (see below).
     if data_args.dataset_name is not None:
+        from datasets import load_from_disk
+        datasets = load_from_disk('/mnt/disks/nlpvnhub/transformers/examples/flax/language-modeling/binhvq_test')
         # Downloading and loading a dataset from the hub.
         datasets = load_dataset(
             data_args.dataset_name,
@@ -628,7 +630,7 @@ def main():
 
     max_seq_length = min(data_args.max_seq_length, tokenizer.model_max_length)
 
-    # use Punkt Sentence Tokenizer to divide a document into a list of sentences
+    # Use Punkt Sentence Tokenizer to divide a document into a list of sentences
     nltk.download('punkt')
     sentence_tokenizer = nltk.data.load("tokenizers/punkt/english.pickle")
     def sentence_split_function(example):
@@ -644,21 +646,46 @@ def main():
         load_from_cache_file=not data_args.overwrite_cache,
     )
 
+    # Tokenize every text, then concatenate them together before splitting them in smaller parts.
+    # Since we make sure that all sequences are of the same length, no attention_mask is needed.
     def tokenize_function(examples):
-        # Remove empty lines
-        return tokenizer(
-            examples[text_column_name],
-            padding="max_length",
-            truncation=True,
-            max_length=max_seq_length,
-            add_special_tokens=True,
-        )
+        return tokenizer(examples[text_column_name], add_special_tokens=True)
 
     tokenized_datasets = splitted_datasets.map(
         tokenize_function,
         batched=True,
         num_proc=data_args.preprocessing_num_workers,
         remove_columns=text_column_name,
+        load_from_cache_file=not data_args.overwrite_cache,
+    )
+
+    # Main data processing function that will concatenate all texts from our dataset and generate chunks of
+    # max_seq_length.
+    def group_texts(examples):
+        # Concatenate all texts.
+        concatenated_examples = {k: list(chain(*examples[k])) for k in examples.keys()}
+        total_length = len(concatenated_examples[list(examples.keys())[0]])
+        # We drop the small remainder, we could add padding if the model supported it instead of this drop, you can
+        # customize this part to your needs.
+        if total_length >= max_seq_length:
+            total_length = (total_length // max_seq_length) * max_seq_length
+        # Split by chunks of max_len.
+        result = {
+            k: [t[i: i + max_seq_length] for i in range(0, total_length, max_seq_length)]
+            for k, t in concatenated_examples.items()
+        }
+        return result
+
+    # Note that with `batched=True`, this map processes 1,000 texts together, so group_texts throws away a
+    # remainder for each of those groups of 1,000 texts. You can adjust that batch_size here but a higher value
+    # might be slower to preprocess.
+    #
+    # To speed up this part, we use multiprocessing. See the documentation of the map method for more information:
+    # https://huggingface.co/docs/datasets/package_reference/main_classes.html#datasets.Dataset.map
+    tokenized_datasets = tokenized_datasets.map(
+        group_texts,
+        batched=True,
+        num_proc=data_args.preprocessing_num_workers,
         load_from_cache_file=not data_args.overwrite_cache,
     )
 
