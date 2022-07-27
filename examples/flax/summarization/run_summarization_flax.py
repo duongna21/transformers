15b13b498a66de46ac1703b07e6ee0f234a15ccb
duongna21 <lablaugrana01@gmail.com>
2022-07-27 09:55:47 +0700

    ahihi

--- examples/flax/summarization/run_summarization_flax.py
+++ examples/flax/summarization/run_summarization_flax.py
@@ -603,8 +603,8 @@ def main():
         print('\nbatch["labels"][:2]: ', model_inputs["labels"][:2])
         print('\nmodel_inputs["labels"][:, :-1]==tokenizer.pad_token_id: ', model_inputs["labels"][:, :-1]==tokenizer.pad_token_id)
         print('eos_pos_mask: ', eos_pos_mask)
-        # eos_pos = np.argwhere(eos_pos_mask==True)
-        # print('eos_pos: ', eos_pos)
+        eos_pos = np.argwhere(eos_pos_mask==True)
+        print('eos_pos: ', eos_pos)
         # model_inputs["labels"][eos_pos] = tokenizer.eos_token_id
         # print('\nprocessed batch["labels"][:2]: ', tokenizer.batch_decode(model_inputs["labels"][:2]))
         # print('\nbatch["decoder_input_ids"][:2]: ', tokenizer.batch_decode(model_inputs["decoder_input_ids"][:2]))
