8e6a2a673c6557a2693c4ff5d56f3e71c6ece700
duongna21 <lablaugrana01@gmail.com>
2022-08-03 16:30:52 +0700

    ahihi

--- examples/flax/language-modeling/run_mlm_flax.py
+++ examples/flax/language-modeling/run_mlm_flax.py
@@ -717,10 +717,10 @@ def main():
             loss = loss.sum()
             num_labels = label_mask.sum()
 
-            return loss, num_labels
+            return loss/num_labels
 
-        grad_fn = jax.value_and_grad(loss_fn, has_aux=True)
-        (loss, num_labels), grad = grad_fn(state.params)
+        grad_fn = jax.value_and_grad(loss_fn, has_aux=False)
+        (loss), grad = grad_fn(state.params)
         grad = jax.lax.pmean(grad, "batch")
         # grad = jax.tree_map(lambda x: x/jax.lax.psum(num_labels), grad)
         # new_state = state.apply_gradients(grads=grad)
