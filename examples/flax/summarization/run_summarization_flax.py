e56018c51856241b08fde5ab0d0993fd99549597
duongna21 <lablaugrana01@gmail.com>
2022-08-10 14:39:18 +0700

    ahihi

--- examples/flax/summarization/run_summarization_flax.py
+++ examples/flax/summarization/run_summarization_flax.py
@@ -879,8 +879,8 @@ def main():
         eval_metrics = []
         eval_preds = []
         eval_labels = []
-
-        eval_loader = data_loader(input_rng, eval_dataset, eval_batch_size, drop_last=False)
+        eval_loader = data_loader(input_rng, predict_dataset, eval_batch_size, drop_last=False)
+        # eval_loader = data_loader(input_rng, eval_dataset, eval_batch_size, drop_last=False)
         eval_steps = math.ceil(len(eval_dataset) / eval_batch_size)
         for _ in tqdm(range(eval_steps), desc="Evaluating...", position=2, leave=False):
             # Model forward
