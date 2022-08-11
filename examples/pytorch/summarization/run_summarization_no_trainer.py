c9c1e70bd16e536a75347ad710bb6e340363b6ab
duongna21 <lablaugrana01@gmail.com>
2022-08-11 15:34:46 +0700

    ahihi

--- examples/pytorch/summarization/run_summarization_no_trainer.py
+++ examples/pytorch/summarization/run_summarization_no_trainer.py
@@ -475,10 +475,10 @@ def main():
         inputs = examples[text_column]
         targets = examples[summary_column]
         inputs = [prefix + inp for inp in inputs]
-        model_inputs = tokenizer(inputs, max_length=args.max_source_length, padding=padding, truncation=True)
+        model_inputs = tokenizer(inputs, max_length=args.max_source_length, padding=padding, truncation=True, return_token_type_ids=False)
 
         # Tokenize targets with the `text_target` keyword argument
-        labels = tokenizer(text_target=targets, max_length=max_target_length, padding=padding, truncation=True)
+        labels = tokenizer(text_target=targets, max_length=max_target_length, padding=padding, truncation=True, return_token_type_ids=False)
 
         # If we are padding here, replace all tokenizer.pad_token_id in the labels by -100 when we want to ignore
         # padding in the loss.
