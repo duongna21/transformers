7d76b300ed5761a2ce99878344b3d15f9881ddde
duongna21 <lablaugrana01@gmail.com>
2023-01-04 15:38:07 +0700

    enable xformers gpt

--- src/transformers/models/clip/modeling_clip.py
+++ src/transformers/models/clip/modeling_clip.py
@@ -289,7 +289,8 @@ class CLIPAttention(nn.Module):
         src_len = key_states.size(1)
 
         if self._use_memory_efficient_attention_xformers:
-            print(f"\n\ncausal_attention_mask: {causal_attention_mask}")
+            print(f"\n\ncausal_attention_mask: {causal_attention_mask.shape, causal_attention_mask}")
+            print(f"\n\nattention_mask: {attention_mask.shape, attention_mask}")
             attn_output = self._memory_efficient_attention_xformers(query_states, key_states, value_states, attn_bias=attention_mask, p=self.dropout)
         else:
             attn_weights = torch.bmm(query_states, key_states.transpose(1, 2))
