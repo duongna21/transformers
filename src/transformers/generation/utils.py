bbf440a48d70e148b25c51aaf4b619dcd652b841
duongna21 <lablaugrana01@gmail.com>
2023-01-06 20:37:30 +0700

    enable xformers gpt

--- src/transformers/generation/utils.py
+++ src/transformers/generation/utils.py
@@ -2132,7 +2132,7 @@ class GenerationMixin:
                 continue  # don't waste resources running the code we don't need
 
             next_token_logits = outputs.logits[:, -1, :]
-            # print(f"next_token_logits: {next_token_logits, next_token_logits.shape}")
+            print(f"next_token_logits: {next_token_logits, next_token_logits.shape}")
 
             # pre-process distribution
             next_tokens_scores = logits_processor(input_ids, next_token_logits)
