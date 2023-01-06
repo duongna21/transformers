7a5a8d604547730be750dd9de7ed25e80dd73192
duongna21 <lablaugrana01@gmail.com>
2023-01-06 23:02:26 +0700

    enable xformers gpt

--- src/transformers/models/gpt2/modeling_gpt2.py
+++ src/transformers/models/gpt2/modeling_gpt2.py
@@ -360,7 +360,7 @@ class GPT2Attention(nn.Module):
         return outputs  # a, present, (attentions)
 
     def _memory_efficient_attention_xformers(self, query, key, value, p=0):
-        hidden_states = xformers.ops.memory_efficient_attention(query, key, value, attn_bias=xformers.ops.LowerTriangularMask(), p=p)
+        hidden_states = xformers.ops.memory_efficient_attention(query, key, value, attn_bias=xformers.ops.LowerTriangularMask())
         return hidden_states
 
 class GPT2MLP(nn.Module):
